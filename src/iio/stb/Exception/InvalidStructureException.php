6ab59ccedd4793e22ea93015f960d47e33c65106
iio <hannes.forsgard@gmail.com>
2013-01-31 16:01:33 +0100

    Added new year in copyright claim

--- src/iio/stb/Exception/InvalidStructureException.php
+++ src/iio/stb/Exception/InvalidStructureException.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the stb package
  *
- * Copyright (c) 2012 Hannes Forsgård
+ * Copyright (c) 2012-13 Hannes Forsgård
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
