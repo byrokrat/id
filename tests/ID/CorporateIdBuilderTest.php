effb14a625dfc6e2a126689911d8b39f0d191132
iio <hannes.forsgard@gmail.com>
2013-12-24 17:41:53 +0100

    Licensed under GPL [ci skip]

--- tests/ID/CorporateIdBuilderTest.php
+++ tests/ID/CorporateIdBuilderTest.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * This file is part of Swedish-Technical-Bureaucracy.
+ *
+ * Copyright (c) 2012-14 Hannes Forsgård
+ *
+ * Swedish-Technical-Bureaucracy is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Swedish-Technical-Bureaucracy is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Swedish-Technical-Bureaucracy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace iio\stb\ID;
 
 class CorporateIdBuilderTest extends \PHPUnit_Framework_TestCase
