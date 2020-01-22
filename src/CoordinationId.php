0f891af62f39d743809c9a0d61abf163bbdf0cbf
Hannes Forsgård <hannes.forsgard@fripost.org>
2020-01-22 10:04:44 +0100

    Account for century when parsing date of birth

--- src/CoordinationId.php
+++ src/CoordinationId.php
@@ -24,15 +24,17 @@ class CoordinationId extends PersonalId
     {
         list(, $century, $this->datestr, $delim, $serialPost, $check) = NumberParser::parse(self::PATTERN, $raw);
 
-        $dateOfBirth = DateTimeCreator::createFromFormat(
-            'ymd',
-            str_pad(
-                (string)(intval($this->datestr) - 60),
-                6,
-                '0',
-                STR_PAD_LEFT
-            )
-        );
+        // remove 60 and preserve left side zeros
+        $compensatedDatestr = str_pad((string)(intval($this->datestr) - 60), 6, '0', STR_PAD_LEFT);
+        $compensatedFormat = 'ymd';
+
+        if ($century) {
+            $compensatedDatestr = $century . $compensatedDatestr;
+            $compensatedFormat = 'Ymd';
+        }
+
+        // parse a date of birth even though datestr may refer a date that does not actually exist
+        $dateOfBirth = DateTimeCreator::createFromFormat($compensatedFormat, $compensatedDatestr);
 
         parent::__construct($century . $dateOfBirth->format('ymd') . $delim . $serialPost . $check);
     }
